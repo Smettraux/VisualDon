20909c7efdecd7e050465d4a36224a784593b827
Smettraux <mettrauxsteve@gmail.com>
2021-04-29 21:35:45 +0200

    exo 2 update

--- 20210423/carte-leaflet/src/index.js
+++ 20210423/carte-leaflet/src/index.js
@@ -23,53 +23,5 @@ bars.features.map(d => {
   
   const [lon, lat] = d.geometry.coordinates;
   console.log(d.geometry.coordinates);
-  L.marker([lat, lon], { icon }).addTo(map)
+  L.marker([lat, lon], { icon }).bindPopup(d.properties.name || d.properties['addr:street'] || d.properties.uid).addTo(map)
 })
-
-// L.geoJSON(
-//   bars,
-//   {
-//     style: feature =>
-//       feature.properties['name'] === 'Black Lodge'
-//           ? { color: 'indianred' }
-//           : { color: 'steelblue' },
-//       onEachFeature: (feature, layer) =>
-//         layer.bindPopup(feature.properties.name || feature.properties['addr:street'] || feature.properties.uid)
-//   }
-// ).addTo(map)
-
-
-
-// const map = L.map('map').setView([46.78104, 6.64707], 17)
-
-// L.tileLayer(
-//   'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}',
-//   {
-//     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//     subdomains: 'abcd',
-//     minZoom: 0,
-//     maxZoom: 20,
-//     ext: 'png',
-//   })
-//     .addTo(map)
-
-
-
-// arbres.map(d => {
-//   const [lon, lat] = d
-//   L.marker([lat, lon], { icon }).addTo(map)
-// })
-
-// L.geoJSON(
-//   batiments,
-//   {
-//     style: feature =>
-//       feature.properties['name'] === 'Centre Saint-Roch'
-//         ? { color: 'indianred' }
-//         : { color: 'steelblue' },
-//     onEachFeature: (feature, layer) =>
-//       layer.bindPopup(feature.properties.name || feature.properties['addr:street'] || feature.properties.uid)
-      
-//   },
-// ).addTo(map)
-
